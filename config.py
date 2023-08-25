f134883dfa47e4eeb78ccbb7d70fd0241f708068
wchy1993 <35326128+wchy1993@users.noreply.github.com>
2023-08-25 21:17:51 +0900

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ heads = (3, 6, 12, 24)
 window_sizes = ((4, 8, 16, 32), (4, 8, 16), (4, 8), (4, 8))
 
 # Training configuration
-num_epochs = 10
-train_batch_size = 32
-val_batch_size = 32
-learning_rate = 0.001
+num_epochs = 200
+train_batch_size = 64
+val_batch_size = 64
+learning_rate = 0.1
 weight_decay = 0.05
 traindir = '/imagenet/train'
 valdir = '/imagenet/val'
